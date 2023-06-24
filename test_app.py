2a62f270be69f9c9ddb81d1e63fe64716d44bec3
madheena85 <137288777+madheena85@users.noreply.github.com>
2023-06-24 22:49:52 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,8 +1,6 @@
 import streamlit
 import snowflake.connector
 
-
-
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('snowflake')
 streamlit.text('Advanced concepts')
@@ -10,3 +8,10 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
